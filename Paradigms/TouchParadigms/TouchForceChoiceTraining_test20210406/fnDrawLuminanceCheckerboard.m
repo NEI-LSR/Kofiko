1c70f67fc98274f358206f502892fc969150690e
Rig A User <conwaycolorlab@gmail.com>
2021-04-28 12:23:22 -0400

    deleted old backups

--- Paradigms/TouchParadigms/TouchForceChoiceTraining_test20210406/fnDrawLuminanceCheckerboard.m
+++
@@ -1,8 +0,0 @@
-function fnDrawLuminanceCheckerboard(hWindow, strctCurrentTrial)
-global g_strctPTB                                                                                                                                                                                                                  
-if g_strctPTB.m_bRunningOnStimulusServer
-	Screen('FillRect',hWindow, strctCurrentTrial.m_aiPixelGridLUTIndices, strctCurrentTrial.m_strctChoiceVars.checkerboardCoord);
-else
-    Screen('FillRect',hWindow, strctCurrentTrial.m_strctChoicePeriod.m_aiLocalLuminanceCheckerboard, strctCurrentTrial.m_strctChoiceVars.checkerboardCoord);
-end
-return;
